b2ffa9191f1ca704f2837cfac9de532303e0904d
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-08-10 19:46:14 -0500

    Agregar archivos de proyecto.

---
+++ PersonaValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionDatosEntrada
+{
+   public class PersonaValidator:AbstractValidator<Persona>
+    {
+        public PersonaValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ingrese el nombre");
+            RuleFor(x => x.LastName).Length(3,20).WithMessage("La longitud del apallido debe de estar de 3 a 20 caracteres");
+            RuleFor(x => x.Edad).ExclusiveBetween(1, 100).WithMessage("La edad no es valida");
+            //===validacion personalizado====
+            RuleFor(x => x.Edad).Must(edad=>edad>18).WithMessage("El alumno no es mayor de dedad");//validacion perzonalizada con un predicado
+            RuleFor(x => x.Edad).Must(validarPersonalizado).WithMessage("El alumno no es mayor de dedad v2");//validacion perzonalizada con un metodo
+        }
+        public Boolean validarPersonalizado(int edad)
+        {            
+            return edad>18;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionDatosEntrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Descargar de nuget "fluentvalidation" version 8.6.2
+            Persona p = new Persona() { 
+             Name=null,
+             LastName="Gonzalez",
+             Edad=14             
+            };
+            PersonaValidator validator = new PersonaValidator();
+            var results = validator.Validate(p);
+            if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    Console.WriteLine(item);
+                }                
+            }
+            Console.WriteLine("Terminado");
+            Console.ReadLine();
+
+        }
+    }
+}
